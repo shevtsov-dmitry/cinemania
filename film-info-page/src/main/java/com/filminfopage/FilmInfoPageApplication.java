3302eb15f292c591b75a68c1edf41a468aba9e47
shevtsov dmitry <shadowstakeus28@gmail.com>
2023-10-20 13:58:16 +0300

    initial commit

---
+++ film-info-page/src/main/java/com/filminfopage/FilmInfoPageApplication.java
@@ -0,0 +1,13 @@
+package com.filminfopage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmInfoPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmInfoPageApplication.class, args);
+	}
+
+}
---
+++ film-info-page/src/test/java/com/filminfopage/FilmInfoPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filminfopage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmInfoPageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
