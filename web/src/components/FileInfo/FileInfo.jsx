7af6caec7c63757ac54a364dd52547bdea37a554
shevtsov-dmitry <shadowstakeus28@gmail.com>
2023-10-25 23:35:32 +0300

    feat: create and desig test form for adding films

--- web/src/components/FileInfo/FileInfo.jsx
+++ web/src/components/FileInfo/FileInfo.jsx
@@ -1,12 +1,66 @@
-function FileInfo(){
-    return(
-        <>
-            <h1>hello world</h1>
-            <form action="">
+function FileInfo() {
+    return (<>
+            <div className="container">
+                <div className="add-film-header">add film</div>
+                <form action="">
+                    <ul>
+                        <li>
+                            <div className="p-right-aligner">
+                                <p>film name</p>
+                            </div>
+                            <input type="text"/>
+                        </li>
+                        <li>
+                            <div className="p-right-aligner">
+                                <p>country</p>
+                            </div>
+                            <input type="text"/>
+                        </li>
+                        <li>
+                            <div className="p-right-aligner">
+                                <p>release date</p>
+                            </div>
 
-            </form>
-        </>
-    )
+                            <input type="text"/>
+                        </li>
+                        <li>
+                            <div className="p-right-aligner">
+                                <p>genre</p>
+                            </div>
+
+                            <input type="text"/>
+                        </li>
+                        <li>
+                            <div className="p-right-aligner">
+                                <p>minimal age</p>
+                            </div>
+                            <input type="text"/>
+                        </li>
+                        <li>
+                            <div className="p-right-aligner">
+                                <p>poster</p>
+                            </div>
+                            <input type="text"/>
+                        </li>
+                        <li>
+                            <div className="p-right-aligner">
+                                <p>watch time</p>
+                            </div>
+                            <input type="text"/>
+                        </li>
+                        <li>
+                            <div className="p-right-aligner">
+                                <p>rating</p>
+                            </div>
+                            <input type="text"/>
+                        </li>
+                    </ul>
+                    <div className="button-aligner">
+                        <button id="add-film-button">submit</button>
+                    </div>
+                </form>
+            </div>
+        </>)
 }
 
 export default FileInfo
