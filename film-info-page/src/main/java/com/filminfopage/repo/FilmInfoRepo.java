edbc7ba18da5ae236ee727f81caaea6b31f29dfd
shevtsov-dmitry <shadowstakeus28@gmail.com>
2023-10-25 17:51:40 +0300

    fix: repo params swap

--- film-info-page/src/main/java/com/filminfopage/repo/FilmInfoRepo.java
+++ film-info-page/src/main/java/com/filminfopage/repo/FilmInfoRepo.java
@@ -3,6 +3,6 @@ package com.filminfopage.repo;
 import com.filminfopage.entity.FilmInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FilmInfoRepo extends JpaRepository<Long, FilmInfo> {
+public interface FilmInfoRepo extends JpaRepository<FilmInfo, Long> {
 
 }
