2ffcf144eee9678f27b18a38bafd759b45a57aa8
shevtsov-dmitry <shadowstakeus28@gmail.com>
2023-10-26 01:21:00 +0300

    feat: gather form info on submit button click

--- web/src/components/FileInfo/FileInfo.jsx
+++ web/src/components/FileInfo/FileInfo.jsx
@@ -1,58 +1,106 @@
+import {useState} from "react";
+
 function FileInfo() {
+
+    const [formData, setFormData] = useState({
+        filmName: '',
+        country: '',
+        releaseDate: '',
+        genre: '',
+        minimalAge: '',
+        imageUrl: '',
+        watchTime: '',
+        rating: '',
+    })
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const jsonData = JSON.stringify(formData)
+        console.log(jsonData);
+    }
+
     return (<>
             <div className="container">
                 <div className="add-film-header">add film</div>
-                <form action="">
+                <form onSubmit={handleSubmit}>
                     <ul>
                         <li>
                             <div className="p-right-aligner">
                                 <p>film name</p>
                             </div>
-                            <input type="text"/>
+                            <input type="text"
+                                   name="filmName"
+                                   value={formData.filmName}
+                                   onChange={handleInputChange}/>
                         </li>
                         <li>
                             <div className="p-right-aligner">
                                 <p>country</p>
                             </div>
-                            <input type="text"/>
+                            <input type="text"
+                                   name="country"
+                                   value={formData.country}
+                                   onChange={handleInputChange}/>
                         </li>
                         <li>
                             <div className="p-right-aligner">
                                 <p>release date</p>
                             </div>
 
-                            <input type="text"/>
+                            <input type="text"
+                                   name="releaseDate"
+                                   value={formData.releaseDate}
+                                   onChange={handleInputChange}/>
                         </li>
                         <li>
                             <div className="p-right-aligner">
                                 <p>genre</p>
                             </div>
-
-                            <input type="text"/>
+                            <input type="text"
+                                   name="genre"
+                                   value={formData.genre}
+                                   onChange={handleInputChange}/>
                         </li>
                         <li>
                             <div className="p-right-aligner">
                                 <p>minimal age</p>
                             </div>
-                            <input type="text"/>
+                            <input type="text"
+                                   name="minimalAge"
+                                   value={formData.minimalAge}
+                                   onChange={handleInputChange}/>
                         </li>
                         <li>
                             <div className="p-right-aligner">
                                 <p>poster</p>
                             </div>
-                            <input type="text"/>
+                            <input type="text"
+                                   name="imageUrl"
+                                   value={formData.imageUrl}
+                                   onChange={handleInputChange}/>
                         </li>
                         <li>
                             <div className="p-right-aligner">
                                 <p>watch time</p>
                             </div>
-                            <input type="text"/>
+                            <input type="text"
+                                   name="watchTime"
+                                   value={formData.watchTime}
+                                   onChange={handleInputChange}/>
                         </li>
                         <li>
                             <div className="p-right-aligner">
                                 <p>rating</p>
                             </div>
-                            <input type="text"/>
+                            <input type="text"
+                                   name="rating"
+                                   value={formData.rating}
+                                   onChange={handleInputChange}/>
                         </li>
                     </ul>
                     <div className="button-aligner">
