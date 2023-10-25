66f7f0f4411a5da3269ef80bff551f930a4088de
shevtsov-dmitry <shadowstakeus28@gmail.com>
2023-10-25 15:40:06 +0300

    add: JUnit dependency for testing

--- film-info-page/src/test/java/com/filminfopage/FilmInfoPageApplicationTests.java
+++ film-info-page/src/test/java/com/filminfopage/FilmInfoPageApplicationTests.java
@@ -1,13 +1,29 @@
 package com.filminfopage;
 
+import com.filminfopage.entity.FilmInfo;
+import org.junit.gen5.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Calendar;
+import java.util.Date;
+
 @SpringBootTest
 class FilmInfoPageApplicationTests {
+    @Test
+    void insertFullFilmInfoOnce() {
+        FilmInfo film = new FilmInfo(
+                "film",
+                new Date(2012, Calendar.JUNE, 20),
+                "USA",
+                "horror",
+                18,
+                "url/myImage/url",
+                "1:30:05",
+                5.4F
+        );
 
-	@Test
-	void contextLoads() {
-	}
+        Assertions.assertNotEquals(film.getId(), 1, "did not create film info");
+    }
 
 }
