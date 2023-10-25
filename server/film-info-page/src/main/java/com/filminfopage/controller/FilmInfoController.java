18b14628e7d0f901a9383ef4cc6b574562eefeea
shevtsov-dmitry <shadowstakeus28@gmail.com>
2023-10-26 02:36:50 +0300

    feat: save data from json into database

--- server/film-info-page/src/main/java/com/filminfopage/controller/FilmInfoController.java
+++ server/film-info-page/src/main/java/com/filminfopage/controller/FilmInfoController.java
@@ -1,4 +1,26 @@
 package com.filminfopage.controller;
 
+import com.filminfopage.entity.FilmInfo;
+import com.filminfopage.repo.FilmInfoRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/add-new-film-info")
 public class FilmInfoController {
+
+
+    private final FilmInfoRepo filmInfoRepo;
+
+    public FilmInfoController(FilmInfoRepo filmInfoRepo) {
+        this.filmInfoRepo = filmInfoRepo;
+    }
+
+    @GetMapping
+    public boolean saveData(@RequestBody FilmInfo filmInfo){
+
+        FilmInfo saved = filmInfoRepo.save(filmInfo);
+
+        return true;
+    }
 }
