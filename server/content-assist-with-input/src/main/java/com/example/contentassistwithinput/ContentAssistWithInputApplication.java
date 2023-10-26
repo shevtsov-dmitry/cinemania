c89321e6c8327bb1abe16f32789ed683d8452880
shevtsov-dmitry <shadowstakeus28@gmail.com>
2023-10-26 11:51:44 +0300

    add: initialize content assist module

---
+++ server/content-assist-with-input/src/main/java/com/example/contentassistwithinput/ContentAssistWithInputApplication.java
@@ -0,0 +1,13 @@
+package com.example.contentassistwithinput;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentAssistWithInputApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContentAssistWithInputApplication.class, args);
+    }
+
+}
---
+++ server/content-assist-with-input/src/test/java/com/example/contentassistwithinput/ContentAssistWithInputApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.contentassistwithinput;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentAssistWithInputApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
