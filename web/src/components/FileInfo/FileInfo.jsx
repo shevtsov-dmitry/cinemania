b38053d8d27f8a7617f11fb01927218d2d38f71e
shevtsov-dmitry <shadowstakeus28@gmail.com>
2023-11-04 12:44:11 +0300

    fix, style: change form style structure.
    Include more classes. Get rid of inheritance to allow inserting suggestions form

--- web/src/components/FileInfo/FileInfo.jsx
+++ web/src/components/FileInfo/FileInfo.jsx
@@ -12,7 +12,9 @@ function FileInfo() {
     const [rating, setRating] = useState('')
 
 
-    function fillForm() {
+    function fillForm(e) {
+        e.preventDefault();
+
         const filledForm = {
             filmName: filmName,
             country: country,
@@ -23,98 +25,146 @@ function FileInfo() {
             watchTime: watchTime,
             rating: rating
         }
+
+        console.log(filledForm)
     }
 
-    const handleInputChange = (e) => {
-        const { name, value } = e.target;
-        switch (name) {
-            case 'filmName': setFilmName(value); break;
-            case 'country': setCountry(value); break;
-            case 'releaseDate': setReleaseDate(value); break;
-            case 'genre': setGenre(value); break;
-            case 'minimalAge': setMinimalAge(value); break;
-            case 'imageUrl': setImageUrl(value); break;
-            case 'watchTime': setWatchTime(value); break;
-            case 'rating': setRating(value); break;
+    const handleInputChange = (input) => {
+        const {name, value} = input.target;
+        setValues();
+        showTypingSuggestions(input)
+        console.log(input.target.positionX, input.target.positionY)
+
+
+        function setValues() {
+            switch (name) {
+                case 'filmName':
+                    setFilmName(value);
+                    break;
+                case 'country':
+                    setCountry(value);
+                    break;
+                case 'releaseDate':
+                    setReleaseDate(value);
+                    break;
+                case 'genre':
+                    setGenre(value);
+                    break;
+                case 'minimalAge':
+                    setMinimalAge(value);
+                    break;
+                case 'imageUrl':
+                    setImageUrl(value);
+                    break;
+                case 'watchTime':
+                    setWatchTime(value);
+                    break;
+                case 'rating':
+                    setRating(value);
+                    break;
+            }
         }
+
+
         // console.log(value)
     };
 
+    const showTypingSuggestions = () => {
+
+        return (
+            <ul className="typing-suggestions-ul">
+                <li className="typing-suggestions-li">one</li>
+                <li className="typing-suggestions-li">two</li>
+                <li className="typing-suggestions-li">one</li>
+                <li className="typing-suggestions-li">two</li>
+                <li className="typing-suggestions-li">three</li>
+            </ul>
+        )
+    }
 
     return (
         <div className="container">
             <div className="add-film-header">add film</div>
             <form onSubmit={fillForm}>
-                <ul>
-                    <li>
+                <ul className="form-ul">
+                    <li className="form-li">
                         <div className="p-right-aligner">
                             <p>film name</p>
                         </div>
-                        <input type="text"
+                        <input className="form-input"
+                               type="text"
                                name="filmName"
                                value={filmName}
                                onChange={handleInputChange}/>
                     </li>
-                    <li>
+                    {showTypingSuggestions()}
+                    <li className="form-li">
                         <div className="p-right-aligner">
                             <p>country</p>
                         </div>
-                        <input type="text"
+                        <input className="form-input"
+                               type="text"
                                name="country"
                                value={country}
                                onChange={handleInputChange}/>
                     </li>
-                    <li>
+                    <li className="form-li">
                         <div className="p-right-aligner">
                             <p>release date</p>
                         </div>
-                        <input type="text"
+                        <input className="form-input"
+                               type="text"
                                name="releaseDate"
                                value={releaseDate}
                                onChange={handleInputChange}/>
                     </li>
-                    <li>
+                    <li className="form-li">
                         <div className="p-right-aligner">
                             <p>genre</p>
                         </div>
-                        <input type="text"
+                        <input className="form-input"
+                               type="text"
                                name="genre"
                                value={genre}
                                onChange={handleInputChange}
                         />
                     </li>
-                    <li>
+                    <li className="form-li">
                         <div className="p-right-aligner">
                             <p>minimal age</p>
                         </div>
-                        <input type="text"
+                        <input className="form-input"
+                               type="text"
                                name="minimalAge"
                                value={minimalAge}
                                onChange={handleInputChange}/>
                     </li>
-                    <li>
+                    <li className="form-li">
                         <div className="p-right-aligner">
                             <p>poster</p>
                         </div>
-                        <input type="text"
+                        <input className="form-input"
+                               type="text"
                                name="imageUrl"
                                value={imageUrl}
                                onChange={handleInputChange}/>
                     </li>
-                    <li>
+                    <li className="form-li">
                         <div className="p-right-aligner">
                             <p>watch time</p>
                         </div>
-                        <input type="text"
+                        <input className="form-input"
+                               type="text"
                                name="watchTime"
                                value={watchTime}
                                onChange={handleInputChange}/>
                     </li>
-                    <li>
+                    <li className="form-li">
                         <div className="p-right-aligner">
                             <p>rating</p>
                         </div>
-                        <input type="text"
+                        <input className="form-input"
+                               type="text"
                                name="rating"
                                value={rating}
                                onChange={handleInputChange}/>
