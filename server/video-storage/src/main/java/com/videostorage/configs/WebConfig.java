e2d83f6a080b9cdd9d1e5e0a694fd1ca247888f7
shevtsov-dmitry <shadowstakeus28@gmail.com>
2023-11-22 16:14:21 +0300

    feat: add web CORS policy config

---
+++ server/video-storage/src/main/java/com/videostorage/configs/WebConfig.java
@@ -0,0 +1,18 @@
+package com.videostorage.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("mongodb/gridfs/**")
+                        .allowedOrigins("http://localhost:3000");
+            }
+        };
+    }
+}
