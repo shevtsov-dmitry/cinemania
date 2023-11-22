17f332c0ce6b4e80c5a7537fb7bbb4811bccf001
shevtsov-dmitry <shadowstakeus28@gmail.com>
2023-11-22 16:12:52 +0300

    feat: configure MongoConfig

---
+++ server/video-storage/src/main/java/com/videostorage/configs/MongoConfig.java
@@ -0,0 +1,31 @@
+package com.videostorage.configs;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.gridfs.GridFsTemplate;
+
+@Configuration
+//@EnableMongoRepositories
+public class MongoConfig extends AbstractMongoClientConfiguration{
+
+    private final MappingMongoConverter mongoConverter;
+
+    @Autowired
+    public MongoConfig(@Lazy MappingMongoConverter mongoConverter) {
+        this.mongoConverter = mongoConverter;
+    }
+
+    @Bean
+    public GridFsTemplate gridFsTemplate() {
+        return new GridFsTemplate(mongoDbFactory(), mongoConverter);
+    }
+
+    @Override
+    protected String getDatabaseName() {
+        return "video_storage_gridfs";
+    }
+}
