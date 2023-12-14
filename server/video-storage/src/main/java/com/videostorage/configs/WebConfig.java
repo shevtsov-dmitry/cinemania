4a85d72558d9d3a9dd335f32ba4e6bf87911cfa4
shevtsov-dmitry <shadowstakeus28@gmail.com>
2023-12-14 11:41:51 +0300

    feat: update web cors config.

--- server/video-storage/src/main/java/com/videostorage/configs/WebConfig.java
+++ server/video-storage/src/main/java/com/videostorage/configs/WebConfig.java
@@ -2,17 +2,16 @@ package com.videostorage.configs;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("mongodb/gridfs/**")
-                        .allowedOrigins("http://localhost:3000", "http://localhost:5173/");
-            }
-        };
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000", "http://localhost:5173", "http://192.168.0.106:5173/")
+                .allowedMethods("GET");
     }
 }
