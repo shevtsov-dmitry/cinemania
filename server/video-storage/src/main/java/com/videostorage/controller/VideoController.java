0b16b2c257528e460c4b23c89b727a79a079f56c
shevtsov-dmitry <shadowstakeus28@gmail.com>
2023-12-14 11:41:33 +0300

    feat: stream whole as big blob. Allowed to choose video fragment.

--- server/video-storage/src/main/java/com/videostorage/controller/VideoController.java
+++ server/video-storage/src/main/java/com/videostorage/controller/VideoController.java
@@ -4,7 +4,7 @@ import com.videostorage.model.Video;
 import com.videostorage.service.VideoService;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.FileCopyUtils;
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 @RestController
 @RequestMapping("/video")
@@ -40,14 +41,38 @@ public class VideoController {
         return service.getVideo(title);
     }
 
-    @GetMapping(value =  "/delete/one/byTitle/{name}")
-    public String deleteVideo(@PathVariable String name){
+    @GetMapping(value = "/delete/one/byTitle/{name}")
+    public String deleteVideo(@PathVariable String name) {
         return service.deleteVideo(name);
     }
 
     @GetMapping("/stream/{title}")
-    public void streamVideo(@PathVariable String title, HttpServletResponse response) throws Exception {
+    public void streamVideo(@PathVariable String title,
+                            @RequestHeader(value = HttpHeaders.RANGE, required = false) String rangeHeader,
+                            HttpServletResponse response) throws Exception {
         Video video = service.getVideo(title);
+        InputStream videoStream = video.getStream();
+        response.setContentType(MediaType.APPLICATION_OCTET_STREAM_VALUE);
+
+        if (rangeHeader != null && rangeHeader.startsWith("bytes")) {
+            // Parse the range header
+            long[] range = parseRangeHeader(rangeHeader, videoStream.available());
+
+            // Set the appropriate headers for a partial content response
+            response.setStatus(HttpServletResponse.SC_PARTIAL_CONTENT);
+            response.setHeader(HttpHeaders.CONTENT_RANGE, STR."bytes \{range[0]}-\{range[1]}/\{videoStream.available()}");
+            videoStream.skip(range[0]);
+        }
+
         FileCopyUtils.copy(video.getStream(), response.getOutputStream());
+    }
+
+    // Helper method to parse the Range header
+    private long[] parseRangeHeader(String rangeHeader, long videoLength) {
+        String[] rangeValues = rangeHeader.substring("bytes=".length()).split("-");
+        long start = Long.parseLong(rangeValues[0]);
+        long end = rangeValues.length > 1 ? Long.parseLong(rangeValues[1]) : videoLength - 1;
+
+        return new long[]{start, end};
     }
 }
