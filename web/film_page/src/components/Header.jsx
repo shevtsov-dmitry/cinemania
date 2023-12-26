06806e073a6f205cbd618ef505e5469d3f1820c7
shado <shadowstakeus28@gmail.com>
2023-12-26 14:21:46 +0300

    add: support for prettier + tailwind

--- web/film_page/src/components/Header.jsx
+++ web/film_page/src/components/Header.jsx
@@ -1,11 +1,11 @@
 export function Header() {
-  return (
-    <>
-      <header className="site-name w-100 h-10 bg-slate-500 text-white flex justify-between items-center">
-        <div>dark/light mode</div>
-        <image>icon</image>
-        <p>досмотреть</p>
-      </header>
-    </>
-  );
+    return (
+        <>
+            <header className="site-name w-100 site-name w-100 flex  flex  h-10 h-10 items-center items-center justify-between justify-between bg-slate-500  bg-slate-500  text-white text-white">
+                <div>dark/light mode</div>
+                <image>icon</image>
+                <p>досмотреть</p>
+            </header>
+        </>
+    )
 }
