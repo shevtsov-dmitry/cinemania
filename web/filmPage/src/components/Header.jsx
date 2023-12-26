e88e9e2212d1ce41e74d1eab43a6a6be71843ca4
shado <shadowstakeus28@gmail.com>
2023-12-26 13:57:20 +0300

    refactor: change main folders names

--- web/filmPage/src/components/Header.jsx
+++
@@ -1,7 +0,0 @@
-export function Header () {
-    return <>
-        <header className="site-name w-100 h-10 bg-slate-500">
-
-        </header>
-    </>
-}
---
+++ web/film_page/src/components/Header.jsx
@@ -0,0 +1,11 @@
+export function Header() {
+  return (
+    <>
+      <header className="site-name w-100 h-10 bg-slate-500 text-white flex justify-between items-center">
+        <div>dark/light mode</div>
+        <image>icon</image>
+        <p>досмотреть</p>
+      </header>
+    </>
+  );
+}
