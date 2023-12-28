b66713bec0161b8f41623c4c01ced917f757aaa6
shado <shadowstakeus28@gmail.com>
2023-12-28 22:29:00 +0300

    add: config for db MongoConfig.java

---
+++ server/film-info-page/src/main/java/com/filminfopage/config/MongoConfig.java
@@ -0,0 +1,30 @@
+package com.filminfopage.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.gridfs.GridFsTemplate;
+
+@Configuration
+public class MongoConfig extends AbstractMongoClientConfiguration {
+
+    private final MappingMongoConverter mongoConverter;
+
+    @Autowired
+    public MongoConfig(@Lazy MappingMongoConverter mongoConverter) {
+        this.mongoConverter = mongoConverter;
+    }
+
+    @Bean
+    public GridFsTemplate gridFsTemplate() {
+        return new GridFsTemplate(mongoDbFactory(), mongoConverter);
+    }
+
+    @Override
+    protected String getDatabaseName() {
+        return "posters";
+    }
+}
