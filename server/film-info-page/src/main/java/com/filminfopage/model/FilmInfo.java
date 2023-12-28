0431aea37a3acef7834b671d422aeb1f9ae794c9
shado <shadowstakeus28@gmail.com>
2023-12-28 19:10:32 +0300

    feat: integrate lombok to the project

--- server/film-info-page/src/main/java/com/filminfopage/model/FilmInfo.java
+++ server/film-info-page/src/main/java/com/filminfopage/model/FilmInfo.java
@@ -1,10 +1,13 @@
 package com.filminfopage.model;
 
 import jakarta.persistence.*;
+import lombok.*;
 import org.springframework.lang.NonNull;
 
 import java.util.Date;
 
+@Data
+@NoArgsConstructor
 @Entity
 public class FilmInfo {
     @Id
@@ -25,13 +28,9 @@ public class FilmInfo {
     private int age;
     @NonNull
     private String imageUrl;
-    private String watchTime; // TODO make time authentication for uploaded file automatic
+    private String watchTime;
     private float rating;
 
-
-    public FilmInfo() {
-
-    }
 
     public FilmInfo(Long id, @NonNull String name, @NonNull Date releaseDate, @NonNull String country,
                     @NonNull String genre, int age, @NonNull String imageUrl, String watchTime, float rating) {
@@ -68,76 +67,4 @@ public class FilmInfo {
         this.imageUrl = imageUrl;
     }
 
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Date getReleaseDate() {
-        return releaseDate;
-    }
-
-    public void setReleaseDate(Date releaseDate) {
-        this.releaseDate = releaseDate;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public String getGenre() {
-        return genre;
-    }
-
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getWatchTime() {
-        return watchTime;
-    }
-
-    public void setWatchTime(String watchTime) {
-        this.watchTime = watchTime;
-    }
-
-    @NonNull
-    public String getFilmName() {
-        return filmName;
-    }
-
-    public void setFilmName(@NonNull String filmName) {
-        this.filmName = filmName;
-    }
-
-    public float getRating() {
-        return rating;
-    }
-
-    public void setRating(float rating) {
-        this.rating = rating;
-    }
 }
--- server/film-info-page/src/main/java/com/filminfopage/model/Poster.java
+++ server/film-info-page/src/main/java/com/filminfopage/model/Poster.java
@@ -1,8 +1,12 @@
 package com.filminfopage.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.bson.types.Binary;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+@Data
+@NoArgsConstructor
 @Document
 public class Poster {
     private String id;
