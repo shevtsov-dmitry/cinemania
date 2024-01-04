50fb0aaa32e2be017c71626915f70095ac8a97c6
shado <shadowstakeus28@gmail.com>
2024-01-04 14:10:29 +0300

    refactor: decompose large methods to small ones

---
+++ server/video-storage/src/main/java/com/videostorage/constants/ApplicationConstants.java
@@ -0,0 +1,8 @@
+package com.videostorage.constants;
+
+public class ApplicationConstants {
+    public static final int CHUNK_SIZE = 3 * 1024 * 1024; // MB
+
+    private ApplicationConstants() {
+    }
+}
--- server/video-storage/src/main/java/com/videostorage/controller/VideoController.java
+++ server/video-storage/src/main/java/com/videostorage/controller/VideoController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import reactor.core.publisher.Mono;
@@ -22,6 +23,7 @@ public class VideoController {
     private final VideoService service;
 
     Logger log = LoggerFactory.getLogger(this.getClass());
+
     public VideoController(VideoService service) {
         this.service = service;
     }
@@ -38,19 +40,16 @@ public class VideoController {
         }
     }
 
-//    @GetMapping(value = "/get/one/byTitle/{title}", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
-//    public Video sendVideoResponse(@PathVariable String title) throws IOException {
-//        return service.getVideo(title);
-//    }
-
     @GetMapping(value = "/delete/one/byTitle/{name}")
     public String deleteVideo(@PathVariable String name) {
         return service.deleteVideo(name);
     }
 
-    @GetMapping(value = "/stream/{title}", produces = "video/mp4")
-    public Mono<Resource> streamVideo(@PathVariable String title) throws IOException {
-        return service.getVideo(title);
+    // TODO: add support for streaming to mobile devices
+    @GetMapping("/stream/{title}")
+    public Mono<ResponseEntity<byte[]>> streamVideo(@RequestHeader(value = "Range", required = false) String range,
+                                                    @PathVariable String title) {
+        return Mono.just(service.prepareContent(title, range));
     }
 
 }
