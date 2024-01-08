39d50e640ad1c98e5c3a7cf98072e0f38e85bf69
shado <shadowstakeus28@gmail.com>
2024-01-08 16:49:57 +0300

    init: VideoMaterialInfo.jsx

--- web/film_page/src/App.jsx
+++ web/film_page/src/App.jsx
@@ -1,11 +1,13 @@
 import { Header } from './components/Header'
 import { Preview } from './components/Preview'
+import {VideoMaterialInfo} from "./components/VideoMaterialInfo";
 
 export default function App() {
     return (
         <body className="min-w-100 h-dvh min-h-20 bg-neutral-800">
             <Header/>
-            <Preview/>
+            {/*<Preview/>*/}
+        <VideoMaterialInfo/>
         </body>
     )
 }
---
+++ web/film_page/src/components/VideoMaterialInfo.jsx
@@ -0,0 +1,12 @@
+export function VideoMaterialInfo(){
+    const formData ={
+        title: "Heap my phone.",
+        releaseDate: "2022-04-11",
+        country: "Austria",
+        genre: "Thriller",
+        age: 16,
+        posterId: "12efw239Fjasa",
+        videoId: "Fjasa12efw239",
+    }
+
+}
