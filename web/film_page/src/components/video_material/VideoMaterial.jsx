d2b514e0f9906f21210a4b978d77b2b43f15e68d
shado <shadowstakeus28@gmail.com>
2024-01-08 18:27:40 +0300

    init: VideoMaterial page

---
+++ web/film_page/src/components/video_material/VideoMaterial.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const VideoMaterial = () => {
+    return(
+        <body className={"bg-neutral-800"}>
+            <h2 className={"text-white"}>sec page</h2>
+        </body>
+    )
+}
