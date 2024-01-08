34e5db1f8d2d4999720fc0a21e665dc8cceef5dd
shevtsov_dmitry <shadowstakeus28@gmail.com>
2024-01-08 19:34:28 +0300

    style: scrolling preview posters

--- web/film_page/src/components/main/Preview.jsx
+++ web/film_page/src/components/main/Preview.jsx
@@ -1,33 +1,36 @@
 export function Preview() {
     return (
         <>
-            <ul id={"previews-sequence-block"} className="w-full flex gap-4 overflow-scroll">
-                <li className={"preview-poster-el"}></li>
-                <li className={"preview-poster-el"}></li>
-                <li className={"preview-poster-el"}></li>
-                <li className={"preview-poster-el"}></li>
-                <li className={"preview-poster-el"}></li>
-                <li className={"preview-poster-el"}></li>
-                <li className={"preview-poster-el"}></li>
-                <li className={"preview-poster-el"}></li>
-                <li className={"preview-poster-el"}></li>
-                <li className={"preview-poster-el"}></li>
-                <li className={"preview-poster-el"}></li>
-                <li className={"preview-poster-el"}></li>
-                <li className={"preview-poster-el"}></li>
-                <li className={"preview-poster-el"}></li>
-                <li className={"preview-poster-el"}></li>
-                <li className={"preview-poster-el"}></li>
-                <li className={"preview-poster-el"}></li>
-                <li className={"preview-poster-el"}></li>
-                <li className={"preview-poster-el"}></li>
-                <li className={"preview-poster-el"}></li>
-                <li className={"preview-poster-el"}></li>
-                <li className={"preview-poster-el"}></li>
-                <li className={"preview-poster-el"}></li>
-                <li className={"preview-poster-el"}></li>
-                <li className={"preview-poster-el"}></li>
-            </ul>
+            <h3 className={"text-white text-2xl font-bold p-2"}>Новинки</h3>
+            <div className="overflow-x-scroll bg-blue-500 p-2 no-scrollbar">
+                <ul id={'previews-sequence-block'} className="flex w-fit gap-4">
+                    <li className={'preview-poster-el redblock'}></li>
+                    <li className={'preview-poster-el'}></li>
+                    <li className={'preview-poster-el'}></li>
+                    <li className={'preview-poster-el'}></li>
+                    <li className={'preview-poster-el'}></li>
+                    <li className={'preview-poster-el'}></li>
+                    <li className={'preview-poster-el'}></li>
+                    <li className={'preview-poster-el'}></li>
+                    <li className={'preview-poster-el'}></li>
+                    <li className={'preview-poster-el'}></li>
+                    <li className={'preview-poster-el'}></li>
+                    <li className={'preview-poster-el'}></li>
+                    <li className={'preview-poster-el'}></li>
+                    <li className={'preview-poster-el'}></li>
+                    <li className={'preview-poster-el'}></li>
+                    <li className={'preview-poster-el'}></li>
+                    <li className={'preview-poster-el'}></li>
+                    <li className={'preview-poster-el'}></li>
+                    <li className={'preview-poster-el'}></li>
+                    <li className={'preview-poster-el'}></li>
+                    <li className={'preview-poster-el'}></li>
+                    <li className={'preview-poster-el'}></li>
+                    <li className={'preview-poster-el'}></li>
+                    <li className={'preview-poster-el'}></li>
+                    <li className={'preview-poster-el'}></li>
+                </ul>
+            </div>
         </>
     )
 }
