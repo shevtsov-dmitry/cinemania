ec4134feb12f484f3ad49ff90d5acd659f40ccb7
shado <shadowstakeus28@gmail.com>
2024-01-09 12:12:47 +0300

    style: design left and right arrows for posters sequence

--- web/film_page/src/components/main/Preview.jsx
+++ web/film_page/src/components/main/Preview.jsx
@@ -1,35 +1,25 @@
+function fillBlockWithPosters() {
+    const content = []
+    for (let i = 0; i < 15; i++) {
+        content.push(<li className={'preview-poster-el'}></li>)
+    }
+    return content
+}
+
 export function Preview() {
     return (
         <>
-            <h3 className={"text-white text-2xl font-bold p-2"}>Новинки</h3>
-            <div className="overflow-x-scroll bg-blue-500 p-2 no-scrollbar">
+            <h3 className={'p-2 text-2xl font-bold text-white'}>Новинки</h3>
+            <div className="no-scrollbar relative overflow-x-scroll bg-blue-500 p-2">
                 <ul id={'previews-sequence-block'} className="flex w-fit gap-4">
-                    <li className={'preview-poster-el redblock'}></li>
-                    <li className={'preview-poster-el'}></li>
-                    <li className={'preview-poster-el'}></li>
-                    <li className={'preview-poster-el'}></li>
-                    <li className={'preview-poster-el'}></li>
-                    <li className={'preview-poster-el'}></li>
-                    <li className={'preview-poster-el'}></li>
-                    <li className={'preview-poster-el'}></li>
-                    <li className={'preview-poster-el'}></li>
-                    <li className={'preview-poster-el'}></li>
-                    <li className={'preview-poster-el'}></li>
-                    <li className={'preview-poster-el'}></li>
-                    <li className={'preview-poster-el'}></li>
-                    <li className={'preview-poster-el'}></li>
-                    <li className={'preview-poster-el'}></li>
-                    <li className={'preview-poster-el'}></li>
-                    <li className={'preview-poster-el'}></li>
-                    <li className={'preview-poster-el'}></li>
-                    <li className={'preview-poster-el'}></li>
-                    <li className={'preview-poster-el'}></li>
-                    <li className={'preview-poster-el'}></li>
-                    <li className={'preview-poster-el'}></li>
-                    <li className={'preview-poster-el'}></li>
-                    <li className={'preview-poster-el'}></li>
-                    <li className={'preview-poster-el'}></li>
+                    {fillBlockWithPosters()}
                 </ul>
+                <div className="centered-arrow-block centered-arrow-block-left">
+                    <img src={"icons/left-arrow.png"} className={'arrow'}  alt={"scroll left"}/>
+                </div>
+                <div className="centered-arrow-block centered-arrow-block-right w-fit h-fit">
+                    <img src={"icons/right-arrow.png"} className={'arrow'}  alt={"scroll right"}/>
+                </div>
             </div>
         </>
     )
