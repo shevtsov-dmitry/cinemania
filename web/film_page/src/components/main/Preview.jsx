72b31b33ea390ecd988954ce36e2bd1946a5cc47
shado <shadowstakeus28@gmail.com>
2024-01-10 16:58:03 +0300

    style: smooth scrolling of poster blocks in sequence

--- web/film_page/src/components/main/Preview.jsx
+++ web/film_page/src/components/main/Preview.jsx
@@ -1,31 +1,41 @@
-const SIZE = {
-    POSTER_HEIGHT: 'h-80',
-}
+import {useRef} from "react";
 
 export function Preview() {
+    const blockRef = useRef()
+
+    const ARROW_SCROLL_DISTANCE = 800
+    function scrollLeft() {
+        if(blockRef.current) {
+            blockRef.current.scrollLeft -= ARROW_SCROLL_DISTANCE;
+        }
+    }
+    function scrollRight() {
+        if(blockRef.current) {
+            blockRef.current.scrollLeft += ARROW_SCROLL_DISTANCE;
+        }
+    }
+
     return (
         <>
             <h3 className={'p-2 text-2xl font-bold text-white'}>Новинки</h3>
             <div id="previews-sequence-block">
-                <div className="no-scrollbar relative overflow-x-scroll p-2">
+                <div ref={blockRef}
+                     className="no-scrollbar relative overflow-x-scroll p-2 scroll-smooth">
                     <ul className="flex w-fit gap-4">
                         {fillBlockWithPosters()}
                     </ul>
                 </div>
                 <div
-                    className={
-                        `arrows-holder w-dvw absolute mt-[-20.5rem] ` +
-                        SIZE.POSTER_HEIGHT
-                    }
+                    className={`arrows-holder w-dvw absolute -mt-[20.5rem] hidden h-[20rem] md:block`}
                 >
-                    <div className="centered-arrow left-8">
+                    <div className="centered-arrow left-8" onClick={scrollLeft}>
                         <img
                             src={'icons/left-arrow.png'}
                             className={'arrow'}
                             alt={'scroll left'}
                         />
                     </div>
-                    <div className="centered-arrow right-0">
+                    <div className="centered-arrow right-0" onClick={scrollRight}>
                         <img
                             src={'icons/right-arrow.png'}
                             className={'arrow'}
@@ -45,7 +55,7 @@ function fillBlockWithPosters() {
             <li
                 key={i}
                 className={
-                    `z-10 w-60 rounded-3xl bg-indigo-900 transition-all ${SIZE.POSTER_HEIGHT} ` +
+                    `z-10 h-80 w-60 rounded-3xl bg-indigo-900 transition-all ` +
                     'hover:scale-105 hover:cursor-pointer '
                 }
             ></li>
