fd8eb55e250911f272bb62ceda549d3437ef6bf0
shado <shadowstakeus28@gmail.com>
2024-01-11 13:15:23 +0300

    refactor: delete VideoMaterial component from home react app

--- web/home/src/components/video_material/VideoMaterial.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const VideoMaterial = () => {
-    return(
-        <div className={"bg-neutral-800"}>
-            <h2 className={"text-white"}>sec page</h2>
-        </div>
-    )
-}
