0ead5aed3488b752e6244ae755a641e810b3144b
shado <shadowstakeus28@gmail.com>
2024-02-04 17:43:41 +0300

    refactor: general code restructures and package renames

--- server/video-material/src/main/java/com/video_material/video/repo/VideoMaterialRepo.java
+++
@@ -1,8 +0,0 @@
-package com.video_material.video.repo;
-
-import com.video_material.video.model.VideoMaterial;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VideoMaterialRepo extends JpaRepository<VideoMaterial, Long> {
-}
