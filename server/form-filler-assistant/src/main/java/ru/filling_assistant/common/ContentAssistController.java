9fef1d9df1d4b98e7f56a714aac44c66c779677e
dmitry <shadowstakeus28@gmail.com>
2024-02-16 18:10:05 +0300

    feat: rewrite abstract controller for content assist feature

--- server/form-filler-assistant/src/main/java/ru/filling_assistant/common/ContentAssistController.java
+++ server/form-filler-assistant/src/main/java/ru/filling_assistant/common/ContentAssistController.java
@@ -1,70 +1,39 @@
 package ru.filling_assistant.common;
 
-import com.google.gson.Gson;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Component
-public class ContentAssistController<T extends AbstractNameableEntity> {
-    private final Class<T> aClass;
-    private final ContentAssistService<T> service;
-    private final Gson gson = new Gson();
+public abstract class ContentAssistController<T extends Nameable> {
 
-    public ContentAssistController(Class<T> aClass, ContentAssistService<T> service) {
-        this.aClass = aClass;
+    private final ContentAssistService<T> service;
+
+    protected ContentAssistController(ContentAssistService<T> service) {
         this.service = service;
     }
 
-    public ResponseEntity<String> addOne(String name) {
-        if (name.isBlank()) {
+    public ResponseEntity<String> tryToSaveOneEntity(T country) {
+        final String countryName = country.getName();
+        if (countryName.isBlank()) {
             return ResponseEntity.badRequest().body("incoming parameter data is empty.");
         }
+
         try {
-            T entity = aClass.getDeclaredConstructor().newInstance();
-            entity.setName(name);
-            return saveOneAndGetId(entity);
+            T saved = service.save(country);
+            return ResponseEntity.ok(saved.toString());
         } catch (Exception e) {
-            return ResponseEntity.internalServerError().body("Couldn't create instance of the class on server.");
+            final List<T> oneContryList = new ArrayList<>(List.of(country));
+            return ResponseEntity.ok(service.saveWithoutDuplicates(oneContryList).getFirst().toString());
         }
     }
 
-    private ResponseEntity<String> saveOneAndGetId(T entity) {
+    public ResponseEntity<List<T>> tryToSaveListOfEntities(List<T> entities) {
+        entities.removeIf(country -> country.getName().isBlank());
         try {
-            return ResponseEntity.ok(service.save(entity).toString());
-        } catch (Exception e) {
-            final List<T> extensibleMonoList = new ArrayList<>(List.of(entity));
-            String json = gson.toJson(service.saveWithoutDuplicates(extensibleMonoList));
-            return ResponseEntity.badRequest().body(json);
-        }
-    }
-
-    public ResponseEntity<String> addMany(List<String> entityNamesList) {
-        entityNamesList.removeIf(String::isBlank);
-        List<T> entities = new ArrayList<>(entityNamesList.size());
-        for (String name : entityNamesList) {
-            try {
-                T entity = aClass.getDeclaredConstructor().newInstance();
-                entity.setName(name);
-                entities.add(entity);
-            } catch (Exception e) {
-                return ResponseEntity.internalServerError().body("Couldn't create new instance on server side.");
-            }
-        }
-        try {
-            return ResponseEntity.ok(gson.toJson(service.saveNewEntities(entities)));
+            return ResponseEntity.ok(service.saveNewEntities(entities));
         } catch (UnsupportedOperationException e) {
-            return ResponseEntity.ok(gson.toJson(service.saveWithoutDuplicates(entities)));
+            return ResponseEntity.ok(service.saveWithoutDuplicates(entities));
         }
-    }
-
-    public ResponseEntity<String> getBySequence(String sequence) {
-        return ResponseEntity.ok("");
-    }
-
-    public ResponseEntity<String> delete(List<String> entityNamesList) {
-        return ResponseEntity.ok("");
     }
 }
