15541d3b7e1c230cc5787c1e1ea0488fa237c5ef
shevtsov-dmitry <shadowstakeus28@gmail.com>
2024-03-04 18:48:50 +0300

    refactor: remove Video class abstraction

--- server/video-crud-and-streaming/video-material/src/main/java/ru/video_material/repo/BinaryVideoRepo.java
+++
@@ -1,9 +0,0 @@
-package ru.video_material.repo;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-import ru.video_material.model.Video;
-
-@Repository
-public interface BinaryVideoRepo extends MongoRepository<Video, String> {
-}
