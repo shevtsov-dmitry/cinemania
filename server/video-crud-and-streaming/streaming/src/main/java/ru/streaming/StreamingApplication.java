00842ea6099839883440ba5030cf619e726a7350
shevtsov-dmitry <shadowstakeus28@gmail.com>
2024-03-05 12:13:53 +0300

    fix: streaming application launch

--- server/video-crud-and-streaming/streaming/src/main/java/ru/streaming/StreamingApplication.java
+++ server/video-crud-and-streaming/streaming/src/main/java/ru/streaming/StreamingApplication.java
@@ -2,6 +2,9 @@ package ru.streaming;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.util.Assert;
+
+import static java.lang.StringTemplate.STR;
 
 @SpringBootApplication
 public class StreamingApplication {
