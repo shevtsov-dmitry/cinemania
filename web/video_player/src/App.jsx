ed6833c3c0ef7042c9dda4f7f422f00a73f3ba8a
shevtsov-dmitry <shadowstakeus28@gmail.com>
2024-03-19 17:03:37 +0300

    refactor: delete separated not used components

--- web/video_player/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import {VideoPlayer} from "./components/VideoPlayer";
-
-export default function App() {
-    return (
-        <body className="min-w-100 h-dvh min-h-20 bg-neutral-800">s
-            <VideoPlayer/>
-        </body>
-    )
-}
--- web/video_player/src/store/store.js
+++
@@ -1,8 +0,0 @@
-import videoReducer from "./videoReducer";
-
-
-const rootReducer = combineReducers({
-    video: videoReducer,
-})
-
-export const store = createStore(rootReducer)
