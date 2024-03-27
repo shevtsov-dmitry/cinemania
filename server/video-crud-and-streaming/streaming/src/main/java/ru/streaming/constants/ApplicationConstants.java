d5274fdba118bae17ae4860a6b0320e666161fcd
shevtsov-dmitry <shadowstakeus28@gmail.com>
2024-03-27 14:18:14 +0300

    refactor: remove unused modules in streaming service

--- server/video-crud-and-streaming/streaming/src/main/java/ru/streaming/constants/ApplicationConstants.java
+++ server/video-crud-and-streaming/streaming/src/main/java/ru/streaming/constants/ApplicationConstants.java
@@ -6,7 +6,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class ApplicationConstants {
     public static final int CHUNK_SIZE = 3 * 1024 * 1024; // MB
-    public static final String MONGODB_NAME = "video_storage_gridfs";
+    public static final String BUCKET_NAME = "video_storage_gridfs";
 
 
 }
--- server/video-crud-and-streaming/streaming/src/main/java/ru/streaming/repo/VideoRepo.java
+++
@@ -1,8 +0,0 @@
-package ru.streaming.repo;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import ru.streaming.model.Video;
-
-public interface VideoRepo extends MongoRepository<Video, String> {
-
-}
