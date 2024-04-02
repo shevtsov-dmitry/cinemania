71014fbe70c9c4fc809298cff74f402dfde092f4
shevtsov-dmitry <shadowstakeus28@gmail.com>
2024-04-02 11:57:24 +0300

    feat: remove mongo gridFS video upload. Instead upload to file system.

--- server/video-crud-and-streaming/video-material/src/main/java/ru/video_material/service/VideoService.java
+++ server/video-crud-and-streaming/video-material/src/main/java/ru/video_material/service/VideoService.java
@@ -1,33 +1,26 @@
 package ru.video_material.service;
 
-import com.mongodb.client.gridfs.model.GridFSFile;
-import org.bson.codecs.configuration.CodecProvider;
-import org.bson.codecs.configuration.CodecRegistry;
-import org.bson.codecs.pojo.PojoCodecProvider;
-import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.data.mongodb.gridfs.GridFsOperations;
-import org.springframework.data.mongodb.gridfs.GridFsTemplate;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.Assert;
 import org.springframework.web.multipart.MultipartFile;
+import ru.video_material.StaticValues;
 import ru.video_material.model.ContentMetadata;
 import ru.video_material.repo.MetadataRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Objects;
 
-import static com.mongodb.MongoClientSettings.getDefaultCodecRegistry;
 import static java.lang.StringTemplate.STR;
 import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
 import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
@@ -35,48 +28,9 @@ import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
 @Service
 public class VideoService {
 
-    private final MetadataRepo metadataRepo;
-    private final GridFsTemplate gridFsTemplate;
-    private final GridFsOperations operations;
-
-
     @Autowired
-    public VideoService(MetadataRepo metadataRepo, GridFsTemplate gridFsTemplate, GridFsOperations operations) {
-        this.metadataRepo = metadataRepo;
-        this.gridFsTemplate = gridFsTemplate;
-        this.operations = operations;
-    }
+    private MetadataRepo metadataRepo;
 
-//    // codecs are used to support current date time storage
-//    CodecProvider codecProvider = PojoCodecProvider.builder().automatic(true).build();
-//    CodecRegistry codecRegistry = fromRegistries(getDefaultCodecRegistry(), fromProviders(codecProvider));
-    // ### VIDEO
-    public String saveVideo(MultipartFile file) throws IOException {
-        ObjectId savedFileId = gridFsTemplate.store(
-                file.getInputStream(),
-                file.getOriginalFilename(),
-                Objects.requireNonNull(file.getContentType())
-        );
-        return savedFileId.toString();
-    }
-    public byte[] downloadVideoById(String id) throws IOException {
-        GridFSFile foundFile = gridFsTemplate.findOne(new Query(Criteria.where("_id").is(id)));
-        Assert.notNull(foundFile, "Video-material service. File is null in downloadVideoById(String id) when try to retrieve it from database.");
-        var outputStream = new ByteArrayOutputStream();
-        operations.getResource(Objects.requireNonNull(foundFile)).getInputStream().transferTo(outputStream);
-        return outputStream.toByteArray();
-    }
-
-    public String deleteVideoById(String id) {
-        final Query query = new Query(Criteria.where("_id").is(id));
-        gridFsTemplate.delete(query);
-        if (gridFsTemplate.findOne(query) != null) {
-            throw new IllegalArgumentException(STR."Deletion failed. The video with id \{id} doesn't exist in GridFS.");
-        }
-        return id;
-    }
-
-    // ### METADATA
     public ResponseEntity<String> saveMetadata(ContentMetadata contentMetadata) {
         contentMetadata.setCreatedAt(LocalDateTime.now());
         var httpHeaders = new HttpHeaders();
@@ -108,5 +62,16 @@ public class VideoService {
         return ResponseEntity.ok("");
     }
 
-
+    public String uploadVideo(MultipartFile file) {
+        String fileName = file.getOriginalFilename();
+        Path uploadDirectory = Paths.get(StaticValues.HLS_VIDEO_PACKAGE_PATH);
+        try (InputStream inputStream = file.getInputStream()) {
+            Path filePath = uploadDirectory.resolve(fileName);
+            Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);
+            return STR."File uploaded successfully: \{fileName}";
+        } catch (IOException e) {
+            throw new RuntimeException(STR."Failed to upload file: \{fileName}", e);
+        }
+    }
 }
+
