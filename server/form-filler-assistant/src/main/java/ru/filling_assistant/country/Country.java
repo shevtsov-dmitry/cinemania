5c16594285cb1fe0f2476e8d6cb9ad99dd867548
shevtsov-dmitry <shadowstakeus28@gmail.com>
2024-04-04 13:15:55 +0300

    fix: delombok all project

--- server/form-filler-assistant/src/main/java/ru/filling_assistant/country/Country.java
+++ server/form-filler-assistant/src/main/java/ru/filling_assistant/country/Country.java
@@ -1,14 +1,9 @@
 package ru.filling_assistant.country;
 
 import jakarta.persistence.*;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
 import ru.filling_assistant.common.Nameable;
 
 @Entity
-@Data
-@NoArgsConstructor
 public class Country implements Nameable {
 
     @Id
@@ -18,7 +13,27 @@ public class Country implements Nameable {
     @Column(unique = true)
     private String name;
 
+    public Country() {
+
+    }
+
     public Country(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
         this.name = name;
     }
 
