c241b5ce7ef1b4190d8daca068318064dcc90aa3
shevtsov-dmitry <shadowstakeus28@gmail.com>
2024-04-24 13:38:17 +0300

    feat: add FFMPEG library

--- server/video-crud-and-streaming/streaming/src/main/java/ru/streaming/constants/ApplicationConstants.java
+++ server/video-crud-and-streaming/streaming/src/main/java/ru/streaming/constants/ApplicationConstants.java
@@ -4,6 +4,6 @@ public class ApplicationConstants {
     // TODO make it env
     public static final int DOWNLOAD_CHUNK_SIZE = 3 * 1024 * 1024; // MB
     public static final String BUCKET_NAME = "video_storage_gridfs";
-    public static final String VIDEO_STORAGE_PATH = "/run/media/shd/HDD/NginxVideos/";
+    public static final String VIDEO_STORAGE_PATH = "/mnt/HDD/NginxVideos/";
 
 }
