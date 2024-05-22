fd92d10a4cbe286261048b9d0abed0e5c48afdc4
shevtsov-dmitry <shadowstakeus28@gmail.com>
2024-05-22 10:57:42 +0300

    fix: catch and process some exceptions on client if server unavailable

--- web/src/components/Main/Home.jsx
+++ web/src/components/Main/Home.jsx
@@ -1,5 +1,4 @@
 import { Preview } from './Preview'
-import { Header } from './Header'
 import { DefaultCompilation } from './compilations/DefaultCompilation'
 
 export default function Home() {
--- web/src/components/Main/Preview.jsx
+++ web/src/components/Main/Preview.jsx
@@ -1,53 +1,14 @@
 import { useEffect, useRef, useState } from 'react'
-import { SideScrollArrow } from '../common/util/SideScrollArrow'
 import { useSelector } from 'react-redux'
 import { Poster, PosterClass } from './Poster'
+import { SideArrows } from '../common/util/SideArrows'
 
 export function Preview() {
-    // *** EDGE SCREEN ARROWS
-    const scrollableBlockRef = useRef()
-    const rightArrowRef = useRef()
-    const leftArrowRef = useRef()
-    const arrowsHolder = useRef()
-
-    const [isPostersBlockHovered, setIsPostersBlockHovered] = useState(false)
     const [posters, setPosters] = useState([])
 
-    const Arrow = new SideScrollArrow(scrollableBlockRef)
-    const ARROW_SCROLL_DISTANCE = 800
-    const scrollLeft = () => Arrow.scrollLeft(ARROW_SCROLL_DISTANCE)
-    const scrollRight = () => Arrow.scrollRight(ARROW_SCROLL_DISTANCE)
-    const hideArrowsLeaningScreen = () =>
-        Arrow.hideArrowsLeaningScreen(leftArrowRef, rightArrowRef)
-    const hideShowArrowsOnHover = () =>
-        Arrow.hideShowArrowsOnHover(isPostersBlockHovered, arrowsHolder)
-
-    useEffect(() => {
-        if (!isPlayerOpened) {
-            leftArrowRef.current.style.visibility = 'hidden'
-        }
-    }, [])
-
-    useEffect(() => {
-        hideShowArrowsOnHover()
-    }, [isPostersBlockHovered])
-
-    useEffect(() => {
-        const blockElement = scrollableBlockRef.current
-
-        if (blockElement) {
-            blockElement.addEventListener('scroll', hideArrowsLeaningScreen)
-        }
-
-        return () => {
-            if (blockElement) {
-                blockElement.removeEventListener(
-                    'scroll',
-                    hideArrowsLeaningScreen
-                )
-            }
-        }
-    }, [])
+    // const [isPostersBlockHovered, setIsPostersBlockHovered] = useState(false)
+    const scrollableBlockRef = useRef()
+    const postersLoadingTextRef = useRef()
 
     // *** PREVIEW PANEL
     const videoPlayerState = useSelector((state) => state.videoPlayer)
@@ -64,10 +25,24 @@ export function Preview() {
                 console.error(
                     `can't fetch ${process.env.REACT_APP_SERVER_URL}:8080/posters/get/recent/${postersAmountToDisplay}`
                 )
+                postersLoadingTextRef.current.textContent =
+                    'Постерам не удалось загрузиться. Сервер не доступен.'
             })
-            const fetchedMaps = await response.json()
+            if (response === undefined) return
+            const fetchedMaps = await response.json().catch(() => {
+                console.error(
+                    `couldn't parse JSON from /posters/get/recent/ request`
+                )
+                postersLoadingTextRef.current.textContent =
+                    'Постеры не удалось преобразовать.'
+                return
+            })
+            setPosters(collectPosterComponents(fetchedMaps))
+            setIsPostersLoaded(true)
+        }
 
-            const posterList = []
+        function collectPosterComponents(fetchedMaps) {
+            const posterList = new Array(fetchedMaps.length)
             for (const map of fetchedMaps) {
                 for (const k in map) {
                     if (k === 'poster') continue
@@ -78,10 +53,11 @@ export function Preview() {
                     map[k] = parsedBase64
                 }
                 const poster = new PosterClass(map)
-                posterList.push(<Poster posterObject={poster} />)
+                posterList.push(
+                    <Poster key={poster.metadataId} posterObject={poster} />
+                )
             }
-            setPosters(posterList)
-            setIsPostersLoaded(true)
+            return posterList
         }
 
         function base64ToUtf8(ASCII_parsed_text) {
@@ -99,8 +75,8 @@ export function Preview() {
         <>
             <div
                 id="previews-sequence-block"
-                onMouseEnter={() => setIsPostersBlockHovered(true)}
-                onMouseLeave={() => setIsPostersBlockHovered(false)}
+                // onMouseEnter={() => setIsPostersBlockHovered(true)}
+                // onMouseLeave={() => setIsPostersBlockHovered(false)}
             >
                 <div
                     ref={scrollableBlockRef}
@@ -110,37 +86,17 @@ export function Preview() {
                         {isPostersLoaded ? (
                             posters
                         ) : (
-                            <p className="text-1xl font-bold text-white opacity-20">
+                            <p
+                                className="text-1xl font-bold text-white opacity-20"
+                                ref={postersLoadingTextRef}
+                            >
                                 Постеры загружаются...
                             </p>
                         )}
                     </ul>
                 </div>
-                <div
-                    ref={arrowsHolder}
-                    className={`arrows-holder absolute -mt-[20.5rem] h-[20rem] w-dvw `}
-                >
-                    <div className="centered-arrow left-8" onClick={scrollLeft}>
-                        <img
-                            ref={leftArrowRef}
-                            src={'icons/left-arrow.png'}
-                            className={'arrow max-md:w-0 max-md:bg-transparent'}
-                            alt={'scroll left'}
-                        />
-                    </div>
-                    <div
-                        className="centered-arrow right-0"
-                        onClick={scrollRight}
-                    >
-                        <img
-                            ref={rightArrowRef}
-                            src={'icons/right-arrow.png'}
-                            className={'arrow max-md:w-0 max-md:bg-transparent'}
-                            alt={'scroll right'}
-                        />
-                    </div>
-                </div>
             </div>
+            <SideArrows scrollableBlockRef={scrollableBlockRef} />
         </>
     )
 }
