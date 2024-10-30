cca6aa7ea57b15c397607d687ccd2a321b3937d7
shevtsov-dmitry <shadowstakeus28@gmail.com>
2024-10-30 09:52:02 +0300

    feat: temporary show status in development

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,20 +1,54 @@
-import { Header } from './components/Main/Header'
-import { Route, Routes } from 'react-router-dom'
+import {Header} from './components/Main/Header'
+import {Route, Routes} from 'react-router-dom'
 import Home from './components/Main/Home'
-import { VideoPlayer } from './components/VideoPlayer'
-import { useSelector } from 'react-redux'
+import {VideoPlayer} from './components/VideoPlayer'
+import {useSelector} from 'react-redux'
+import {useEffect, useState} from 'react';
 
 export default function App() {
     const videoPlayerState = useSelector((state) => state.videoPlayer)
     const videoId = videoPlayerState.videoId
-    // const videoPlayerPath = `/watch/${videoId}`
+    const [text, setText] = useState('');
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchText = async () => {
+            try {
+                const response = await fetch('https://lovemenot.ru:8443/');
+                if (!response.ok) throw new Error('Failed to fetch data');
+                const text = await response.text();
+                setText(text);
+            } catch (error) {
+                setError(error.message);
+            }
+        };
+
+        fetchText();
+    }, []);
+
     return (
         <div className="min-w-100  h-dvh min-h-20 bg-neutral-800">
-            <Header />
+            <Header/>
             <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/watch/:videoId" element={<VideoPlayer />} />
+                <Route path="/" element={<Home/>}/>
+                <Route path="/watch/:videoId" element={<VideoPlayer/>}/>
             </Routes>
+
+            <div className="flex min-h-screen items-center justify-center bg-gray-900">
+                <div className="max-w-2xl rounded-lg bg-gray-800 p-8 shadow-lg">
+                    <h1 className="mb-4 text-center text-4xl font-bold text-white">
+                        Сообщение с сервера
+                    </h1>
+                    {error ? (
+                        <p className="text-xl text-red-500">{error}</p>
+                    ) : (
+                        <p className="text-center text-2xl leading-relaxed text-white">
+                            {text || 'Загрузка...'}
+                        </p>
+                    )}
+                </div>
+            </div>
+
         </div>
     )
 }
