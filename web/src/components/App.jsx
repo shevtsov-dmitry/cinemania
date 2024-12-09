0c5b587fc63620aeded81d0b0d39583861ce84eb
shevtsov-dmitry <shadowstakeus28@gmail.com>
2024-12-09 21:02:26 +0300

    style: modern design  for save form

--- web/src/components/App.jsx
+++ web/src/components/App.jsx
@@ -1,47 +1,52 @@
-import { StyleSheet, Text, View } from "react-native";
+import {StyleSheet, Text, View} from "react-native";
 
 import Header from "@/src/components/header/Header";
 import Home from "@/src/components/home/Home";
+import FormAddFilm from "@/src/components/admin/form-add-film/FormAddFilm";
 // import { VideoPlayer } from "@/src/components/VideoPlayer";
 
 let error = false;
 
 export default function App() {
-  return (
-    <div className="min-w-100 h-dvh min-h-20 bg-neutral-800">
-      <Header />
-      <Home />
-      {/*<VideoPlayer/>*/}
-      {/*TODO use zustand instead*/}
-      {/*<Routes>*/}
-      {/*    <Route path="/" element={<Home />} />*/}
-      {/*    <Route path="/watch/:videoId" element={<VideoPlayer />} />*/}
-      {/*</Routes>*/}
+    return (
+        <div className="min-w-100 h-dvh min-h-20 bg-neutral-800">
 
-      {/*TODO use different component for this */}
-      <div className="flex min-h-screen items-center justify-center bg-gray-900">
-        <div className="max-w-2xl rounded-lg bg-gray-800 p-8 shadow-lg">
-          <h1 className="mb-4 text-center text-4xl font-bold text-white">
-            Сообщение с сервера
-          </h1>
-          {error ? (
-            <p className="text-xl text-red-500">{error}</p>
-          ) : (
-            <p className="text-center text-2xl leading-relaxed text-white">
-              {"Загрузка..."}
-            </p>
-          )}
+            <div className={"w-dvw h-dvh flex items-center justify-center fixed z-50 bottom-0"}>
+                <FormAddFilm/>
+            </div>
+            <Header/>
+            <Home/>
+            {/*<VideoPlayer/>*/}
+            {/*TODO use zustand instead*/}
+            {/*<Routes>*/}
+            {/*    <Route path="/" element={<Home />} />*/}
+            {/*    <Route path="/watch/:videoId" element={<VideoPlayer />} />*/}
+            {/*</Routes>*/}
+
+            {/*TODO use different component for this */}
+            <div className="flex min-h-screen items-center justify-center bg-gray-900">
+                <div className="max-w-2xl rounded-lg bg-gray-800 p-8 shadow-lg">
+                    <h1 className="mb-4 text-center text-4xl font-bold text-white">
+                        Сообщение с сервера
+                    </h1>
+                    {error ? (
+                        <p className="text-xl text-red-500">{error}</p>
+                    ) : (
+                        <p className="text-center text-2xl leading-relaxed text-white">
+                            {"Загрузка..."}
+                        </p>
+                    )}
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
 });
--- web/src/constants/Colors.ts
+++ web/src/constants/Colors.ts
@@ -2,19 +2,19 @@ const tintColorLight = '#2f95dc';
 const tintColorDark = '#fff';
 
 export default {
-  link: '#2e78b7',
-  light: {
-    text: '#000',
-    background: '#fff',
-    tint: tintColorLight,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorLight,
-  },
-  dark: {
-    text: '#fff',
-    background: '#000',
-    tint: tintColorDark,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorDark,
-  },
+    link: '#2e78b7',
+    light: {
+        text: '#000',
+        background: '#fff',
+        tint: tintColorLight,
+        tabIconDefault: '#ccc',
+        tabIconSelected: tintColorLight,
+    },
+    dark: {
+        text: '#fff',
+        background: '#000',
+        tint: tintColorDark,
+        tabIconDefault: '#ccc',
+        tabIconSelected: tintColorDark,
+    },
 };
