2e6f4004ae0c2d5eb427dc3d006e36d5b0ac5ba1
shevtsov-dmitry <shadowstakeus28@gmail.com>
2025-01-07 21:26:45 +0300

    fix(poster): Parse posters fix.

--- web/src/components/poster/Poster.jsx
+++ web/src/components/poster/Poster.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react";
 
 import ContentMetadata from "@/src/types/ContentMetadata";
 import PosterType from "./PosterType";
@@ -12,65 +12,63 @@ import PosterType from "./PosterType";
  * @returns {JSX.Element}
  */
 export default function Poster({
-  posterType = PosterType.DEFAULT,
-  metadata,
-  base64,
-}) {
-  const [isPosterHovered, setIsPosterHovered] = useState(false);
+                                   posterType = PosterType.DEFAULT,
+                                   metadata,
+                                   base64,
+                               }) {
+    const [isPosterHovered, setIsPosterHovered] = useState(false);
 
-  /**
-   *
-   * @returns {JSX.Element}
-   */
-  const ContentInformation = () => (
-    <>
-      <h3 className="bg-inherit text-3xl font-bold text-white">
-        {metadata.title}
-      </h3>
-      <p className="select-none text-white">{}</p>
-      <p className="select-none text-white">{metadata.country}</p>
-      <p className="select-none text-white">{metadata.mainGenre}</p>
-      <p className="select-none text-white text-xs">{metadata.subGenres}</p>
-      <p className="select-none text-white">{metadata.releaseDate}</p>
-    </>
-  );
+    /**
+     *
+     * @returns {JSX.Element}
+     */
+    const ContentInformation = () => (
+        <>
+            <h3 className="bg-inherit text-3xl font-bold text-white">
+                {metadata.title}
+            </h3>
+            <p className="select-none text-white">{}</p>
+            <p className="select-none text-white">{metadata.country}</p>
+            <p className="select-none text-white">{metadata.mainGenre}</p>
+            <p className="select-none text-white text-xs">{metadata.subGenres}</p>
+            <p className="select-none text-white">{metadata.releaseDate}</p>
+        </>
+    );
 
-  useEffect(() => console.log(base64), []);
-
-  return (
-    <div
-      className={
-        `${posterType.PREVIEW && "h-[200px] w-[150px]"} ` +
-        `${posterType.DEFAULT && "h-96 w-64"} ` +
-        ` z-10 rounded-3xl bg-indigo-900 bg-cover bg-center transition-all hover:scale-105 hover:cursor-pointer`
-      }
-      style={{ backgroundImage: `url(${`data:image/jpeg;base64,/${base64}`})` }}
-      onMouseEnter={() => setIsPosterHovered(true)}
-      onMouseLeave={() => setIsPosterHovered(false)}
-    >
-      <div className="postersInfo">
-        {isPosterHovered && (
-          <div>
-            <ContentInformation />
-            <div className="flex w-64 justify-center">
-              {/* <Link */}
-              {/*     className="absolute bottom-0 z-10 mb-10" */}
-              {/*     to={`/watch/${metadata.videoId}`} */}
-              {/* > */}
-              <button
-                className="select-none rounded-3xl bg-pink-700 p-4 font-sans text-2xl font-bold text-white opacity-75 hover:opacity-95"
-                // onClick={() => {};
-                //     // TODO rewrite to Zustand
-                //     // dispatch(setVideoId(metadata.videoId))
-                // }
-              >
-                Смотреть
-              </button>
-              {/* </Link> */}
-            </div>
-          </div>
-        )}
-      </div>
-    </div>
-  );
+    return (
+        <div
+            className={
+                `${posterType.PREVIEW && "h-[200px] w-[150px]"} ` +
+                `${posterType.DEFAULT && "h-96 w-64"} ` +
+                ` z-10 rounded-3xl bg-indigo-900 bg-cover bg-center transition-all hover:scale-105 hover:cursor-pointer`
+            }
+            style={{backgroundImage: `url(${`data:image/jpeg;base64,${base64}`})`}}
+            onMouseEnter={() => setIsPosterHovered(true)}
+            onMouseLeave={() => setIsPosterHovered(false)}
+        >
+            {/*<div className="postersInfo">*/}
+            {/*    {isPosterHovered && (*/}
+            {/*        <div>*/}
+            {/*            <ContentInformation/>*/}
+            {/*            <div className="flex w-64 justify-center">*/}
+            {/*                /!* <Link *!/*/}
+            {/*                /!*     className="absolute bottom-0 z-10 mb-10" *!/*/}
+            {/*                /!*     to={`/watch/${metadata.videoId}`} *!/*/}
+            {/*                /!* > *!/*/}
+            {/*                <button*/}
+            {/*                    className="select-none rounded-3xl bg-pink-700 p-4 font-sans text-2xl font-bold text-white opacity-75 hover:opacity-95"*/}
+            {/*                    // onClick={() => {};*/}
+            {/*                    //     // TODO rewrite to Zustand*/}
+            {/*                    //     // dispatch(setVideoId(metadata.videoId))*/}
+            {/*                    // }*/}
+            {/*                >*/}
+            {/*                    Смотреть*/}
+            {/*                </button>*/}
+            {/*                /!* </Link> *!/*/}
+            {/*            </div>*/}
+            {/*        </div>*/}
+            {/*    )}*/}
+            {/*</div>*/}
+        </div>
+    );
 }
