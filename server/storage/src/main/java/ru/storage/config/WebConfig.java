99d79dd4a36a45a8170a0d5f394ca4fc1e4c3acc
shevtsov-dmitry <shadowstakeus28@gmail.com>
2025-01-09 12:38:09 +0300

    refactor: move basic html to react native

--- server/storage/src/main/java/ru/storage/config/WebConfig.java
+++ server/storage/src/main/java/ru/storage/config/WebConfig.java
@@ -16,7 +16,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(CLIENT_URL)
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 
