7a7925c220da032d94c6b1443684ed5a42773a78
shevtsov-dmitry <shadowstakeus28@gmail.com>
2025-01-15 13:53:33 +0300

    refactor: move all content creators api into one general class

--- server/storage/src/main/java/ru/storage/content/content_creators/ActorRepo.java
+++
@@ -1,7 +0,0 @@
-package ru.storage.content.content_creators;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ActorRepo extends MongoRepository<Actor, String> {}
--- server/storage/src/main/java/ru/storage/content/content_creators/ContentCreator.java
+++ server/storage/src/main/java/ru/storage/content/content_creators/ContentCreator.java
@@ -1,15 +1,23 @@
 package ru.storage.content.content_creators;
 
+import java.util.List;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.storage.content.ContentDetails;
 
 @Data
-public abstract class ContentCreator {
-
-    protected String fullname;
-    protected String fullnameEng;
-    protected String bornPlace;
-    protected Double heightMeters;
-    protected ContentCreatorKind contentCreatorKind;
-
+@NoArgsConstructor
+@Document
+@AllArgsConstructor
+public class ContentCreator {
+    private String fullname;
+    private String fullnameEng;
+    private String bornPlace;
+    private Double heightMeters;
+    private ContentCreatorKind contentCreatorKind;
+    private List<ContentDetails> filmsParticipated;
 }
