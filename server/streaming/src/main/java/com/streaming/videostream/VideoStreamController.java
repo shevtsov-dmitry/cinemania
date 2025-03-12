f613d0b1a73d133cbcd85a134978424ba1efe07e
dmitry-shevtsov <shadowstakeus28@gmail.com>
2025-03-12 10:50:03 +0300

    refactor: move business logic of video stream into service

--- server/streaming/src/main/java/com/streaming/videostream/VideoStreamController.java
+++ server/streaming/src/main/java/com/streaming/videostream/VideoStreamController.java
@@ -1,68 +1,26 @@
 package com.streaming.videostream;
 
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.*;
-import software.amazon.awssdk.core.ResponseBytes;
-import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 
 @RestController
 @RequestMapping("api/v1/stream")
 public class VideoStreamController {
 
-  private final S3Client s3Client;
-  private static final Logger LOG = LoggerFactory.getLogger(VideoStreamController.class);
+    private final VideoStreamService service;
 
-  @Value("${custom.s3.BUCKET_NAME}")
-  private String bucketName;
-
-  private static final String S3_FOLDER = "videos/trailer/";
-
-  public VideoStreamController(S3Client s3Client) {
-    this.s3Client = s3Client;
-  }
-
-  @GetMapping("{id}/playlist")
-  public void getPlaylist(@PathVariable String id, HttpServletResponse response) {
-    String objectKey = S3_FOLDER + id + "/index.m3u8";
-
-    try {
-      ResponseBytes<?> objectBytes =
-          s3Client.getObjectAsBytes(
-              GetObjectRequest.builder().bucket(bucketName).key(objectKey).build());
-
-      String playlistContent = new String(objectBytes.asByteArray());
-
-      String baseUrl = "http://localhost:8443/api/v1/stream/" + id + "/chunk/";
-      playlistContent = playlistContent.replaceAll("(index\\d+\\.ts)", baseUrl + "$1");
-
-      response.setContentType("application/vnd.apple.mpegurl");
-      response.setHeader(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=\"index.m3u8\"");
-      response.getWriter().write(playlistContent);
-    } catch (IOException e) {
-      LOG.error("Error serving M3U8 playlist: {}", e.getMessage());
+    public VideoStreamController(VideoStreamService service) {
+        this.service = service;
     }
-  }
 
-  @GetMapping("{id}/chunk/{chunkName}")
-  public void getChunk(
-      @PathVariable String id, @PathVariable String chunkName, HttpServletResponse response) {
-    String objectKey = S3_FOLDER + id + "/" + chunkName;
-
-    try {
-      ResponseBytes<?> objectBytes =
-          s3Client.getObjectAsBytes(
-              GetObjectRequest.builder().bucket(bucketName).key(objectKey).build());
-
-      response.setContentType("video/MP2T"); // Correct MIME type for TS chunks
-      response.getOutputStream().write(objectBytes.asByteArray());
-    } catch (IOException e) {
-      LOG.error("Error serving TS chunk: {}", e.getMessage());
+    @GetMapping("{id}/playlist")
+    public void getPlaylist(@PathVariable String id, HttpServletResponse response) {
+        service.getHlsPlaylist(id, response);
     }
-  }
+
+    @GetMapping("{id}/chunk/{chunkName}")
+    public void streamChunk(@PathVariable String id, @PathVariable String chunkName, HttpServletResponse response) {
+        String objectKey = VideoStreamService.S3_FOLDER + id + "/" + chunkName;
+        service.streamChunk(response, objectKey);
+    }
 }
--- server/streaming/src/main/java/com/streaming/videostream/VideoStreamService.java
+++ server/streaming/src/main/java/com/streaming/videostream/VideoStreamService.java
@@ -1,12 +1,63 @@
 package com.streaming.videostream;
 
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.ResponseBytes;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 
-/**
- * VideoStreamService
- */
+import java.io.IOException;
+
 @Service
 public class VideoStreamService {
 
+    @Value("${custom.s3.BUCKET_NAME}")
+    private String bucketName;
+    public static final String S3_FOLDER = "videos/trailer/";
+
+    private final S3Client s3Client;
+    private static final Logger LOG = LoggerFactory.getLogger(VideoStreamService.class);
+
+    public VideoStreamService(S3Client s3Client) {
+        this.s3Client = s3Client;
+    }
+
+    void getHlsPlaylist(String id, HttpServletResponse response) {
+        String objectKey = S3_FOLDER + id + "/index.m3u8";
+
+        try {
+            ResponseBytes<?> objectBytes =
+                    s3Client.getObjectAsBytes(
+                            GetObjectRequest.builder().bucket(bucketName).key(objectKey).build());
+
+            String playlistContent = new String(objectBytes.asByteArray());
+
+            String baseUrl = "http://localhost:8443/api/v1/stream/" + id + "/chunk/";
+            playlistContent = playlistContent.replaceAll("(index\\d+\\.ts)", baseUrl + "$1");
+
+            response.setContentType("application/vnd.apple.mpegurl");
+            response.setHeader(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=\"index.m3u8\"");
+            response.getWriter().write(playlistContent);
+        } catch (IOException e) {
+            LOG.error("Error serving M3U8 playlist: {}", e.getMessage());
+        }
+    }
+
+    public void streamChunk(HttpServletResponse response, String objectKey) {
+        try {
+            ResponseBytes<?> objectBytes =
+                    s3Client.getObjectAsBytes(
+                            GetObjectRequest.builder().bucket(bucketName).key(objectKey).build());
+
+            response.setContentType("video/MP2T");
+            response.getOutputStream().write(objectBytes.asByteArray());
+        } catch (IOException e) {
+            LOG.error("Error serving TS chunk: {}", e.getMessage());
+        }
+    }
 }
