0d763fb52d2166a50453fe71b2b5e3aad5d11479
dmitry-shevtsov <shadowstakeus28@gmail.com>
2025-03-16 07:24:12 +0300

    init: privileges spring boot starter

---
+++ server/privileges/src/main/java/ru/cinemania/privileges/PrivilegesApplication.java
@@ -0,0 +1,13 @@
+package ru.cinemania.privileges;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivilegesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrivilegesApplication.class, args);
+	}
+
+}
---
+++ server/privileges/src/test/java/ru/cinemania/privileges/PrivilegesApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.cinemania.privileges;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrivilegesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
