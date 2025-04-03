7caeaed0c0aac1643bd66a13ba8804b5a2b87f6c
shevtsov-dmitry <shadowstakeus28@gmail.com>
2025-04-03 21:05:45 +0300

    style: update ui for auth form in client

--- web/src/components/auth-screen/AuthScreen.tsx
+++ web/src/components/auth-screen/AuthScreen.tsx
@@ -9,8 +9,8 @@ const AuthScreen = (): ReactElement => {
     return (
         <View className="flex h-full w-full items-center justify-center">
             <View className="absolute h-full w-full bg-black opacity-40 blur" />
-            <View className="h-1/3 w-1/4 rounded-xl bg-white shadow">
-                <Text className="mt-2 text-center text-3xl font-semibold">
+            <View className="flex h-1/3 w-1/4 justify-around rounded-xl border-2 border-solid border-white bg-sky-950 px-2 shadow">
+                <Text className="mt-2 text-center text-3xl font-semibold text-white">
                     Логин
                 </Text>
                 <EmailAuth />
--- web/src/components/auth-screen/EmailAuth.tsx
+++ web/src/components/auth-screen/EmailAuth.tsx
@@ -1,28 +1,41 @@
 import Constants from '@/src/constants/Constants'
 import React, { useState, ReactElement } from 'react'
-import { Button, TextInput, View } from 'react-native'
+import { Button, TextInput, View, Text, Pressable } from 'react-native'
 
 const EmailAuth = (): ReactElement => {
     const [email, setEmail] = useState<string>('')
 
     function handleLogin() {
-        try {
-            fetch(
-                Constants.VERIFICATION_SERVICE_URL + '/api/v1/auth/login/email'
-            )
-        } catch (error) {
-            console.error('Непредвиденная ошибка входа.', error)
-            alert('Не удалось войти из за непредвиденной ошибки', error)
-        }
+        fetch(Constants.VERIFICATION_SERVICE_URL + '/api/v1/auth/login/email', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ email }),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log('Code sent:', data)
+            })
+            .catch((error) => {
+                console.error('Ошибка отправки кода', error)
+                alert('Ошибка отправки кода')
+            })
     }
 
     return (
-        <View className="">
+        <View className="space-y-2">
+            <Text className="text-lg font-semibold text-white">Почта</Text>
             <TextInput
+                className="rounded bg-neutral-100 py-2 text-xl placeholder:pl-2"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
+                placeholder="Введите email"
             />
-            <Button onPress={handleLogin}>Войти</Button>
+            <Pressable
+                className="w-fit rounded-xl bg-blue-500 p-2 text-white shadow"
+                onPress={handleLogin}
+            >
+                <Text className="text-xl font-bold text-white">Войти</Text>
+            </Pressable>
         </View>
     )
 }
